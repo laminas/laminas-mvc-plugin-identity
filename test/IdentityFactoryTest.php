53c038f1676394dc0abc5d4f2e9157dbbf978fd9
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2021-05-10 02:08:47 -0400

    Remove file headers
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- test/IdentityFactoryTest.php
+++ test/IdentityFactoryTest.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/laminas/laminas-mvc-plugin-identity for the canonical source repository
- * @copyright https://github.com/laminas/laminas-mvc-plugin-identity/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas/laminas-mvc-plugin-identity/blob/master/LICENSE.md New BSD License
- */
-
 namespace LaminasTest\Mvc\Plugin\Identity;
 
 use Interop\Container\ContainerInterface;
